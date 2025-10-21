647ea648c20e14949974ea0bb5821f84c99c490c
Roshankumar350 <roshankumar350@gmail.com>
2025-10-22 00:44:58 +0530

    async data support and localhost:8080 integration

--- WishDishApp/ClientResourceLoader/ClientResourceLoader.swift
+++ WishDishApp/ClientResourceLoader/ClientResourceLoader.swift
@@ -14,9 +14,17 @@ enum ResourceLoaderError: Error {
 
 protocol ClientResourceLoading {
     func load<T: Decodable>(_ type: T.Type, from filename: String) throws -> T
+    func loadAsync<T: Decodable>(_ type: T.Type, from urlString: String) async throws -> T
+}
+
+extension ClientResourceLoading {
+    func loadAsync<T: Decodable>(_ type: T.Type, from urlString: String) async throws -> T {
+        throw NSError(domain: "Not implemented", code: 0, userInfo: nil) as? Error  ?? URLError(.badURL)
+    }
 }
 
 struct ClientResourceLoader: ClientResourceLoading {
+
     func load<T: Decodable>(_ type: T.Type, from filename: String) throws -> T {
         guard let url = Bundle.main.url(forResource: filename, withExtension: "json") else {
             throw ResourceLoaderError.fileNotFound
@@ -28,5 +36,19 @@ struct ClientResourceLoader: ClientResourceLoading {
         } catch {
             throw ResourceLoaderError.decodingFailed(error)
         }
+    }
+    
+    func loadAsync<T: Decodable>(_ type: T.Type, from urlString: String) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return try JSONDecoder().decode(T.self, from: data)
     }
 }
